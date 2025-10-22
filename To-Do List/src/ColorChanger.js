618e088f7f17caa3a6d5de460346614984b9db03
Andrea To Chip <andreatochip@yahoo.com>
2025-10-22 23:17:05 +0800

    Update ColorChanger.js

--- To-Do List/src/ColorChanger.js
+++ To-Do List/src/ColorChanger.js
@@ -1,1 +1,13 @@
+export default function TrendingList(props) {
+  return (
+<div className="trending">
+  <h2>Trending Now</h2>
+  <div className="movie-list">
+    {props.movies.map(function(movie) {
+      return <Movie title={movie.title} releaseYear={movie.releaseYear} imageUrl={movie.imageUrl} />
+    })}
+  </div>
+</div>
 
+  );
+}
