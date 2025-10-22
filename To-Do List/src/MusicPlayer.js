d833af4d7cd15832e5d0077f4a03d50e22a5fd75
Andrea To Chip <andreatochip@yahoo.com>
2025-10-22 23:00:24 +0800

    Update MusicPlayer.js

--- To-Do List/src/MusicPlayer.js
+++ To-Do List/src/MusicPlayer.js
@@ -1,1 +1,21 @@
+/*
+import mySong from './Coding/React/first-project/Lukrembo.mp3';
+*/
 
+export default function App() {
+  /*
+  function AudioPlayer({ src }) {
+  const audioRef = useRef(null);
+  
+  
+  const playAudio = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
+  const pauseAudio = () => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+    }
+  */
