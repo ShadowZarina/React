8ca3f4a07b0a72a0a9d4ce5368c38db8d11f02ed
Andrea To Chip <andreatochip@yahoo.com>
2025-11-01 22:46:48 +0800

    Delete Frontend Mentor directory

--- Frontend Mentor/Interactive Rating Component/interactive-rating-component-main/script.js
+++
@@ -1,5 +0,0 @@
-function replaceElement() {
-    let newElement = document.createElement('div');
-    newElement.textContent = 'New Content';
-    document.getElementById('original').replaceWith(newElement);
-}
